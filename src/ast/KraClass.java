e382c43c0031362c7bb517c72490ab82707b95b3
gabrielamartins <gabrielajsmartins@gmail.com>
2015-09-23 23:15:56 -0300

    arrumando KraClass

--- src/ast/KraClass.java
+++ src/ast/KraClass.java
@@ -15,7 +15,23 @@ public class KraClass extends Type {
    private String name;
    private KraClass superclass;
    private InstanceVariableList instanceVariableList;
-   // private MethodList publicMethodList, privateMethodList;
+   private MethodList publicMethodList, privateMethodList;
    // métodos públicos get e set para obter e iniciar as variáveis acima,
    // entre outros métodos
+   
+   public void setPrivateMethodList(MethodList privateMethodList) {
+	   this.privateMethodList = privateMethodList;
+   }
+
+   public void setPublicMethodList(MethodList publicMethodList) {
+	   this.publicMethodList = publicMethodList;
+   }
+
+   public MethodList getPrivateMethodList() {
+	   return privateMethodList;
+   }
+
+   public MethodList getPublicMethodList() {
+	   return publicMethodList;
+   }
 }
