38aae1e74fcaf676408bc72db23f4ed30e469048
valdeirsp <vsperozim@gmail.com>
2015-09-30 19:03:21 -0300

    KraClass: mudança em metodos e criação de searchMethod(). Inclusão de atributos: qualifier;

--- src/ast/KraClass.java
+++ src/ast/KraClass.java
@@ -1,37 +1,71 @@
 package ast;
+
 /*
  * Krakatoa Class
  */
+
+import java.util.Iterator;
+import lexer.Symbol;
+
 public class KraClass extends Type {
 	
-   public KraClass( String name ) {
+   public KraClass( String name, Symbol qualifier, KraClass superClass ) {
       super(name);
+      this.qualifier = qualifier;
+      this.superclass = superClass;
+      this.instanceVariableList = null;
+      this.publicMethodList = new MethodList();
+      this.privateMethodList = new MethodList();
    }
    
    public String getCname() {
       return getName();
    }
    
+   public Symbol getQualifier() {
+	   return qualifier;
+   }
+   
+   public void setInstanceVariableList(InstanceVariableList ivl) {
+	   this.instanceVariableList = ivl;
+   }
+   
+   public void addPublicMethod(Method method) {
+	   
+	   publicMethodList.addElement(method);
+   }
+   
+   public void addPrivateMethod(Method method) {
+	   
+	   privateMethodList.addElement(method); 
+   }
+   
+   public Method searchMethod(Method method) {
+	   
+	   Iterator<Method> it = publicMethodList.elements();
+	   while(it.hasNext()) {
+		   
+		   Method thisMethod = (Method) it.next();
+		   if (thisMethod == method) {
+			   return thisMethod;
+		   }
+	   }
+	   
+	   it = privateMethodList.elements();
+	   while(it.hasNext()) {
+		   
+		   Method thisMethod = (Method) it.next();
+		   if (thisMethod == method) {
+			   return thisMethod;
+		   }
+	   }
+	   
+	   return null;
+   }
+   
    private String name;
    private KraClass superclass;
+   private Symbol qualifier;
    private InstanceVariableList instanceVariableList;
    private MethodList publicMethodList, privateMethodList;
-   // métodos públicos get e set para obter e iniciar as variáveis acima,
-   // entre outros métodos
-   
-   public void setPrivateMethodList(MethodList privateMethodList) {
-	   this.privateMethodList = privateMethodList;
-   }
-
-   public void setPublicMethodList(MethodList publicMethodList) {
-	   this.publicMethodList = publicMethodList;
-   }
-
-   public MethodList getPrivateMethodList() {
-	   return privateMethodList;
-   }
-
-   public MethodList getPublicMethodList() {
-	   return publicMethodList;
-   }
 }
